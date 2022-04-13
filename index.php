da9265bdd41bed67eae164f54486233614d962f4
Josie Flôres <drijosie@gmail.com>
2022-04-12 21:52:54 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adolecente';
+$categorias[] = 'adulto';
+
+//print_r($categorias);
+
+$nome = 'Eduardo';
+$idade = 15;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+{  
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+if($categorias[$i] == 'infantil')
+echo 'O nadador ' .$nome. ' compete na categoria infantil.';
+    }
+
+} else if($idade >= 13 && $idade <= 17 )
+{
+    {  
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+    if($categorias[$i] == 'adolecente')
+    echo 'O nadador ' .$nome. ' compete na categoria adolecente.';
+        }
+}
+}
+else 
+     
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+    if($categorias[$i] == 'adulto')
+    echo 'O nadador ' .$nome. ' compete na categoria adulto.';
+        }
+
+
